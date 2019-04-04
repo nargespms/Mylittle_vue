16764131fca3b5a4abe7318c5f84a4f7e9d61a66
narges <nargespms.np@gmail.com>
2019-04-04 11:42:48 +0430

    search page added

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import homecmp from './homecmp.vue'
+import searchcmp from './searchcmp.vue'
+
+export default [
+  { path: '/', component: homecmp },
+  { path:'/search' ,component : searchcmp }
+]
