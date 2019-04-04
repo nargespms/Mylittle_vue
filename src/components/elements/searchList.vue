df94b74920f7370fac89caf6cc56efadefe0cfc2
narges <nargespms.np@gmail.com>
2019-04-04 14:03:15 +0430

    adding searchForm & little cutom search in searchListNews

--- src/components/elements/searchList.vue
+++ src/components/elements/searchList.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="searchList_wrapper">
     <ul class="searchList">
-      <li v-for="news in newsList" v-bind:key="news._id">
+      <input type="text " v-model="search" placeholder="search news">
+      <li v-for="news in filteredNews" v-bind:key="news._id">
         <h3 class="uptitle">{{news.uptitle}}</h3>
         <h2 class="title">{{news.title}}</h2>
-        <p>{{news.about}}</p>
+        <p>{{news.about | snippet }}</p>
       </li>
     </ul>
 
@@ -18,6 +19,14 @@ export default {
   data() {
     return {
       newsList : newslist,
+      search: '',
+    }
+  },
+  computed: {
+    filteredNews : function(){
+      return this.newsList.filter(news => {
+        return news.title.match(this.search);
+      })
     }
   }
 }
@@ -25,11 +34,14 @@ export default {
 
 <style lang="scss" scoped>
 .searchList_wrapper {
-  width:75%;
-  margin: 24px auto;
-  background-color: #fff;
+  width:60%;
+  margin-top: 24px;
+  float:right;
+  padding-left:16px;
+  box-sizing: border-box;
 }
 .searchList {
+  background-color: #fff;
   padding:8px;
   border:1px solid #1a7a74;
   li {
