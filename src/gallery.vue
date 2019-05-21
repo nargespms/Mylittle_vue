07f78d5aa48d423562e447bb7d9fbda5ff7ac8c4
narges <nargespms.np@gmail.com>
2019-05-21 16:27:47 +0430

    adding gallery

---
+++ src/gallery.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <myheader/>
+    <posts/>
+    <photos/>
+  </div>
+</template>
+
+<script>
+import myheader from './components/header/myheader.vue';
+import posts from './components/elements/posts.vue'
+import photos from './components/elements/photos.vue'
+
+export default {
+   components: {
+      myheader,
+      posts,
+      photos
+    },
+
+}
+</script>
+
